367ba3e3b932b4746873024b179502c6c88a1320
Tom Meier <tom@venombytes.com>
2012-02-10 13:51:34 +1100

    Only play failure effect on first occurence of failure

--- lib/build_light.rb
+++ lib/build_light.rb
@@ -1,4 +1,3 @@
-
 require 'blinky'
 require 'yaml'
 require './lib/jenkins'
@@ -6,28 +5,42 @@ require './lib/jenkins'
 light = Blinky.new.light
 
 begin
+  last_status_file_location = "./config/last_status"
   jenkins_config_file = './config/jenkins.yml'
   config = YAML::load(File.open(jenkins_config_file))
   jenkins = Jenkins.new(config)
 
-  if jenkins.job_statuses.empty?
-    puts 'light: off'
-    light.off!
-  elsif jenkins.has_no_build_failures?
-    puts 'light: success'
-    light.success!
-  elsif jenkins.has_no_unclaimed_builds?
-    puts 'light: warning'
-    light.warning!
-  else
-    puts 'light: failure'
-    light.failure!
-    #Send sound effects
+  job_result = case
+                when jenkins.job_statuses.empty?
+                  'off'
+                when jenkins.has_no_build_failures?
+                  'success'
+                when jenkins.has_no_unclaimed_builds?
+                  'warning'
+                when else
+                  'failure'
+               end
+
+
+  status_file = File.open(last_status_file_location, 'a+')
+  last_status = status_file.readlines.first
+  status_file.close
+
+  puts "Last status : #{last_status}"
+  puts "Setting light : #{job_result}"
+
+  light.__send__("#{job_result}!")
+
+  if job_result == 'failure' && last_status != 'failure'
+    #Play sound effect on first occurence
     puts "Playing failure sound effect"
     `mpg123 ./sounds/nooo.mp3`
   end
+
+  #Setting last_status
+  File.open(last_status_file_location, 'w') {|f| f.write(job_result) } unless last_status == job_result
 rescue StandardError => e
-  puts 'light: off'
+  puts 'Setting light :off'
   light.off!
   raise e
 end
