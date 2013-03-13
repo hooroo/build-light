d9e279170d521f03e59192835c7adbc45e664fca
Daniel Angel-Bradford <locusdelicti@gmail.com>
2013-03-13 17:10:06 +1100

    Adding retro song, beeeatch

--- lib/stand_up.rb
+++ lib/stand_up.rb
@@ -3,6 +3,9 @@
 
 #Standup song
 #30 9 * * 1,2,3,4,5 /bin/bash -c 'export PATH=$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH; RBENV_VERSION=1.9.3-p125; /home/dev/build-light/bin/stand-up >> /home/dev/build-light/log/stand-up.log 2>&1'
+
+require 'Date'
+
 def play_tunes(commands = [])
   return unless commands.size > 0
 
@@ -16,6 +19,7 @@ puts "Playing standup sound effect"
 
 rick_astley       = Random.rand(1 + 50).to_i
 meaning_of_life   = 42
+song_dir = DateTime.now.thursday? ? 'retro' : 'standup'
 
 puts "... Rollin'? : #{rick_astley}"
 
@@ -23,7 +27,7 @@ if rick_astley == meaning_of_life
   dir = File.expand_path('../../sounds/', __FILE__)
   clip = File.join(dir, 'Rick-Astley-Never-Gonna-Give-You-Up.mp3')
 else
-  mp3_directory = File.expand_path('../../sounds/cron_jobs/standup/', __FILE__)
+  mp3_directory = File.expand_path("../../sounds/cron_jobs/#{song_dir}/", __FILE__)
   sound_clips = Dir.glob(File.join(mp3_directory, '*.mp3'))
   clip = sound_clips.sample
 end
